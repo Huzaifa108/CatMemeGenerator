344acd231229783df20f4192312b42195779b939
Huzaifa <huzaifasadiq125@gmail.com>
2023-08-21 00:17:03 +0200

    Initial Commit
    Project created

---
+++ CatMemeGenerator/CatMemeGenerator/CatMemeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatMemeGeneratorApp.swift
+//  CatMemeGenerator
+//
+//  Created by Huzaifa on 19.08.23.
+//
+
+import SwiftUI
+
+@main
+struct CatMemeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
